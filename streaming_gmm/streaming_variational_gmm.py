d40815c0ee83925016737e4acf98a088a2000279
Lukas Zorich <lukas.zorich@gmail.com>
2017-05-09 21:07:04 -0700

    Apply pep8 to streaming_variational_gmm.py

--- streaming_gmm/streaming_variational_gmm.py
+++ streaming_gmm/streaming_variational_gmm.py
@@ -1,6 +1,7 @@
 import numpy as np
 from vb_mgmm.var_bayes_gmm import VariationalGMM
 
+
 class StreamingVariationalGMM:
 
     def __init__(self):
@@ -20,8 +21,10 @@ class StreamingVariationalGMM:
         pass
 
     def update_with_data_shake(self, X_old, X_new):
-        alpha_k_old_data, normal_wishart_params_old_data = self._get_variational_parameters(X_old)
-        alpha_k_new_data, normal_wishart_params_new_data = self._get_variational_parameters(X_new)
+        alpha_k_old_data, normal_wishart_params_old_data = \
+            self._get_variational_parameters(X_old)
+        alpha_k_new_data, normal_wishart_params_new_data = \
+            self._get_variational_parameters(X_new)
         self._dirichlet_update_parameter(alpha_k_old_data, alpha_k_new_data)
         self._normal_wishart_update_parameter(normal_wishart_params_old_data,
                                               normal_wishart_params_new_data)
@@ -36,21 +39,21 @@ class StreamingVariationalGMM:
                                          normal_wishart_params_old_data,
                                          normal_wishart_params_new_data):
         self.beta_k = self._compute_update_beta_k(
-                normal_wishart_params_old_data['beta_k'],
-                normal_wishart_params_new_data['beta_k'])
+            normal_wishart_params_old_data['beta_k'],
+            normal_wishart_params_new_data['beta_k'])
         self.m_k = self._update_m_k(
-                normal_wishart_params_old_data['beta_k_m_k'],
-                normal_wishart_params_new_data['beta_k_m_k'])
+            normal_wishart_params_old_data['beta_k_m_k'],
+            normal_wishart_params_new_data['beta_k_m_k'])
         self._beta_k_m_k = self.beta_k * self.m_k
         self._inv_W_k = self._compute_update_inv_W_k(
-                normal_wishart_params_old_data['inv_W_k_expression'],
-                normal_wishart_params_new_data['inv_W_k_expression'])
+            normal_wishart_params_old_data['inv_W_k_expression'],
+            normal_wishart_params_new_data['inv_W_k_expression'])
         self._inv_W_k_expression = (self._inv_W_k
-                + self.beta_k * np.sum(self.m_k ** 2))
+                                    + self.beta_k * np.sum(self.m_k ** 2))
         self.W_k = np.linalg.inv(self._inv_W_k)
         self.nu_k = self._compute_update_nu_k(
-                normal_wishart_params_old_data['nu_k'],
-                normal_wishart_params_new_data['nu_k'])
+            normal_wishart_params_old_data['nu_k'],
+            normal_wishart_params_new_data['nu_k'])
 
     def _compute_update_m_k(self, beta_k_m_k_old_data, beta_k_m_k_new_data):
         return 1. / self.beta_k * (self._beta_k_m_k
